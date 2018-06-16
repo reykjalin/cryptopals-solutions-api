f11ff9e3bcddb854a3115e91237ea58064ecc105
Kristófer Reykjalín <kristofert13@ru.is>
2018-06-16 01:54:28 -0400

    Add CORS

--- WebApi/src/Startup.cs
+++ WebApi/src/Startup.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -23,6 +25,7 @@ namespace src
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddMvc();
         }
 
@@ -34,6 +37,11 @@ namespace src
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseCors(
+                options => options.WithOrigins("http://localhost:5000").AllowAnyMethod()
+                                                                       .AllowAnyOrigin()
+                                                                       .AllowAnyHeader()
+            );
             app.UseMvc();
         }
     }
