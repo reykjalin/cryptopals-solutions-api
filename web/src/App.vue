1d3b1952eb331b7fdf3165976d76fcac164a3db7
Kristófer Reykjalín <kristofert13@ru.is>
2018-08-10 01:08:56 -0400

    Add navbar

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,10 +1,28 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> | 
-      <router-link to="/base64">base64</router-link>
-    </div>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <nav id="nav" class="navbar" role="navigation" aria-label="main-navigation">
+      <div class="navbar-brand">
+        <router-link class="navbar-item" to="/">
+          <img src="./assets/logo.png" alt="Cryptopals" width="40" height="100">
+        </router-link>
+
+        <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
+          <router-link aria-hidden="true" to="/">Home</router-link>
+          <router-link aria-hidden="true" to="/about">About</router-link> 
+          <router-link aria-hidden="true" to="/base64">base64</router-link>
+        </a>
+      </div>
+
+      <div class="navbar-menu">
+        <!-- navbar start, navbar end -->
+        <router-link class="navbar-item" to="/">Home</router-link>
+        <router-link class="navbar-item" to="/about">About</router-link> 
+        <router-link class="navbar-item" to="/base64">base64</router-link>
+      </div>
+    </nav>
+
     <router-view/>
   </div>
 </template>
